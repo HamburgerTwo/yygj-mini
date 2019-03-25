a8ce4c411c10832415beedde9245182945e78ed2
fanqihang <276636806@qq.com>
2019-03-25 10:30:58 +0800

    update bug

--- src/pages/roleselection/index.tsx
+++ src/pages/roleselection/index.tsx
@@ -7,7 +7,6 @@ import clerk from '../../assets/clerk.png';
 import { ROLE } from '../../constants/user';
 import { isAuthorized } from '../../utils/authorize';
 import './index.scss'
-import withShare from '../../utils/wechatShare'
 // #region 书写注意
 //
 // 目前 typescript 版本还无法在装饰器模式下将 Props 注入到 Taro.Component 中的 props 属性
@@ -36,12 +35,11 @@ type IProps = PageStateProps & PageDispatchProps & PageOwnProps
 interface Index {
   props: IProps;
 }
-@withShare()
+
 @connect(({ user }) => ({
   user
 }), (dispatch) => ({
 }))
-
 class Index extends Component<PageOwnProps,PageState> {
 
   config: Config = {
